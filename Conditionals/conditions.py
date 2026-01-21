c00f58b99e07ace106345978d3d1790d249a16d6
Chipo <chipomafunga211@gmail.com>
2026-01-21 19:55:29 +0200

    Add Conditionals folder

---
+++ Conditionals/conditions.py
@@ -0,0 +1,17 @@
+print("we are learning if elif and else")
+
+#if, elif, ane else
+
+if true:
+    print("Code will run here when true")
+    if False:
+    print("this code will not run")
+
+    age = 20
+    if age > 18:
+        print("you are allowed to vote")
+    else: 
+        print("you are under age")
+
+#What is an index?
+# Each Character in a string has a memory address and it starts with 0.
